3f07abaae5d236567acb7bccfd63bd05936fcb3e
RacyFiend <demonperechemidi@gmail.com>
2025-08-09 14:29:43 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Перчемиди on 09.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
